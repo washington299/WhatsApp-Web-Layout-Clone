7f4c601b6ba3a6d7609424c438055a454334d843
Washington Campos <washingtoncampos9@gmail.com>
2020-07-28 16:31:34 -0300

    refactor: move Header and Home components to its own folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
-import Header from './components/Header';
+import Home from './pages/Home';
 
 import './App.scss';
 
 function App() {
   return (
-    <Header />
+    <Home />
   );
 }
 
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,46 +1,15 @@
 import React from 'react';
 import { RiWhatsappLine } from 'react-icons/ri';
-import { BsThreeDotsVertical } from 'react-icons/bs';
-import { FiSettings } from 'react-icons/fi';
 
 import './styles.scss';
 
 const Header: React.FC = () => (
-  <div className="top-background">
-    <section className="container">
-      <header>
-        <div className="logo">
-          <RiWhatsappLine />
-          <span>Whatsapp Web</span>
-        </div>
-      </header>
-      <main>
-        <h2>To use WhatsApp on your computer:</h2>
-        <ul>
-          <li>Open WhatsApp on your phone.</li>
-          <li>
-            Tap
-            {' '}
-            <b>Menu</b>
-            {' '}
-            <BsThreeDotsVertical />
-            {' '}
-            or
-            {' '}
-            <b>Settings</b>
-            {' '}
-            <FiSettings />
-            {' '}
-            and select
-            {' '}
-            <b>WhatsApp Web.</b>
-          </li>
-          <li>Point your phone to this screen to capture the code.</li>
-        </ul>
-        <a href="/">Need help to get started?</a>
-      </main>
-    </section>
-  </div>
+  <header>
+    <div className="logo">
+      <RiWhatsappLine />
+      <span>Whatsapp Web</span>
+    </div>
+  </header>
 );
 
 export default Header;
