0680a72327fde4a4dd2b9ec93ed58bf1adad88d5
Washington Campos <washingtoncampos9@gmail.com>
2020-08-03 22:00:59 -0300

    chore: set up react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
+import Chat from './pages/Chat';
 
 import './App.scss';
 
 function App() {
   return (
-    <Home />
+    <Router>
+      <Switch>
+        <Route path="/chat" component={Chat} />
+        <Route exact path="/" component={Home} />
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/pages/Chat/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './styles.scss';
+
+const Chat: React.FC = () => (
+  <div>Chat Router</div>
+);
+
+export default Chat;
