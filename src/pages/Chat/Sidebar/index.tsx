aa2c1dde3ffd155c5e4fbc981aa954f9a6b4e4bf
Washington Campos <washingtoncampos9@gmail.com>
2020-08-06 23:55:51 -0300

    feat: add sidebar header styles

--- src/pages/Chat/Sidebar/index.tsx
+++ src/pages/Chat/Sidebar/index.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import { MdDonutLarge, MdChat, MdMoreVert } from 'react-icons/md';
 
 import './styles.scss';
 
 const Sidebar: React.FC = () => (
   <aside className="sidebar">
-    <header className="sidebar__header">Header</header>
+    <header className="sidebar__header">
+      <img
+        src="./assets/images/avatar/avatar-man-1.png"
+        alt="My Profile"
+        className="sidebar__profile-picture"
+      />
+      <div className="sidebar__options">
+        <MdDonutLarge className="options__item" />
+        <MdChat className="options__item" />
+        <MdMoreVert className="options__item" />
+      </div>
+    </header>
     <div className="sidebar__notification">Notification</div>
     <div className="sidebar__search">Search</div>
     <div className="sidebar__contacts">Contact</div>
