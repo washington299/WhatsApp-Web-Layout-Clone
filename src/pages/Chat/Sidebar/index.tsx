340190ef0e4bc34ff6005e4a8646178595f0444d
Washington Campos <washingtoncampos9@gmail.com>
2020-08-09 22:55:29 -0300

    feat: create notification remind component

--- src/pages/Chat/Sidebar/index.tsx
+++ src/pages/Chat/Sidebar/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { MdDonutLarge, MdChat, MdMoreVert } from 'react-icons/md';
+import {
+  MdDonutLarge, MdChat, MdMoreVert, MdNotificationsOff,
+} from 'react-icons/md';
 
 import './styles.scss';
 
@@ -17,7 +19,20 @@ const Sidebar: React.FC = () => (
         <MdMoreVert className="options__item" />
       </div>
     </header>
-    <div className="sidebar__notification">Notification</div>
+
+    <div className="sidebar__notification">
+      <div className="notification__circle">
+        <MdNotificationsOff className="notification__icon" />
+      </div>
+      <div className="notification__texts">
+        <span className="notification__title">Get notified of new messages</span>
+        <span className="notification__button">
+          Turn on Desktop notifications
+          <strong style={{ marginLeft: '3px' }}>&gt;</strong>
+        </span>
+      </div>
+    </div>
+
     <div className="sidebar__search">Search</div>
     <div className="sidebar__contacts">Contact</div>
   </aside>
