59f91d2b85819701a43e864f4b4364a4eb7a3120
Washington Campos <washingtoncampos9@gmail.com>
2020-08-12 23:30:19 -0300

    feat: get contacts details from data.ts

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -1,21 +1,27 @@
 import React from 'react';
 import { MdColorize, MdVolumeOff, MdExpandMore } from 'react-icons/md';
 
+import { IContacts } from '../../interface';
+
 import './styles.scss';
 
-const Contact: React.FC = () => (
+type Props = IContacts;
+
+const Contact: React.FC<Props> = ({
+  name, avatar, lastMessage, timeLastMessage, pinned, mute, unreadMessages,
+}) => (
   <div className="contacts__container">
-    <img src="assets/images/avatar/avatar-man-2.png" alt="Carlos Santana" className="contacts__img" />
+    <img src={avatar} alt={name} className="contacts__img" />
     <div className="contacts__texts">
-      <h3 className="texts__name">Washington Campos</h3>
-      <span className="texts__lastMessage">Ultima mensagem recebida</span>
+      <h3 className="texts__name">{name}</h3>
+      <span className="texts__lastMessage">{lastMessage}</span>
     </div>
     <div className="contacts__details">
-      <span className="details__time">7:30 pm</span>
-      <MdColorize className="details__icon" />
-      <MdVolumeOff className="details__icon" />
-      <MdExpandMore className="details__icon" />
-      <div className="details__unreadMessage">100</div>
+      <span className="details__time">{timeLastMessage}</span>
+      {pinned && <MdColorize className="details__icon" />}
+      {mute && <MdVolumeOff className="details__icon" />}
+      {unreadMessages && <div className="details__unreadMessage">{unreadMessages}</div>}
+      <MdExpandMore className="details__icon options" />
     </div>
   </div>
 );
