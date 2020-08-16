72e943a246c4069a9ca503fa5f6269153c8b7cb5
Washington Campos <washingtoncampos9@gmail.com>
2020-08-16 18:32:14 -0300

    feat: ensure ChatScreen is displayed when clicked on Contacts

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { MdColorize, MdVolumeOff, MdExpandMore } from 'react-icons/md';
 
 import { IContacts } from '../../interface';
+import { useChatScreenDispatch } from '../../contexts/chat-screen';
 
 import './styles.scss';
 
@@ -9,21 +10,37 @@ type Props = IContacts;
 
 const Contact: React.FC<Props> = ({
   name, avatar, lastMessage, timeLastMessage, pinned, mute, unreadMessages,
-}) => (
-  <div className="contacts__container">
-    <img src={avatar} alt={name} className="contacts__img" />
-    <div className="contacts__texts">
-      <h3 className="texts__name">{name}</h3>
-      <span className="texts__lastMessage">{lastMessage}</span>
+}) => {
+  const [hidden, setHidden] = useState<boolean>(!!unreadMessages);
+  const dispatch = useChatScreenDispatch();
+
+  function handleClick() {
+    dispatch({
+      type: 'DISPLAY',
+    });
+    setHidden(false);
+  }
+
+  return (
+    <div className="contacts__container" onClick={handleClick}>
+      <img src={avatar} alt={name} className="contacts__img" />
+      <div className="contacts__texts">
+        <h3 className="texts__name">{name}</h3>
+        <span className="texts__lastMessage">{lastMessage}</span>
+      </div>
+      <div className="contacts__details">
+        <span className="details__time">{timeLastMessage}</span>
+        {pinned && <MdColorize className="details__icon" />}
+        {mute && <MdVolumeOff className="details__icon" />}
+        {hidden && (
+          <div className="details__unreadMessage">
+            {unreadMessages}
+          </div>
+        )}
+        <MdExpandMore className="details__icon options" />
+      </div>
     </div>
-    <div className="contacts__details">
-      <span className="details__time">{timeLastMessage}</span>
-      {pinned && <MdColorize className="details__icon" />}
-      {mute && <MdVolumeOff className="details__icon" />}
-      {unreadMessages && <div className="details__unreadMessage">{unreadMessages}</div>}
-      <MdExpandMore className="details__icon options" />
-    </div>
-  </div>
-);
+  );
+};
 
 export default Contact;
--- src/pages/Chat/Body/index.tsx
+++ src/pages/Chat/Body/index.tsx
@@ -1,13 +1,24 @@
 import React from 'react';
 
+import { useChatScreenState } from '../../../contexts/chat-screen';
+
 import './styles.scss';
 
-const Body: React.FC = () => (
-  <section className="body">
-    <header className="body__header">Body header</header>
-    <section className="body__messages">Body messages</section>
-    <div className="body__text-area">Body text area</div>
-  </section>
-);
+const Body: React.FC = () => {
+  const { display } = useChatScreenState();
+
+  return (
+    <section className="body">
+      {display && (
+        <>
+          <header className="body__header">Body header</header>
+          <section className="body__messages">Body messages</section>
+          <div className="body__text-area">Body text area</div>
+        </>
+      )}
+      <h1>Welcome</h1>
+    </section>
+  );
+};
 
 export default Body;
