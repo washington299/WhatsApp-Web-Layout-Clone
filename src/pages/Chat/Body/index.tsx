c3d4f52254ee6de19b3f23c3d8331ead34346a77
Washington Campos <washingtoncampos9@gmail.com>
2020-08-16 22:18:42 -0300

    feat: create body initial screen

--- src/pages/Chat/Body/index.tsx
+++ src/pages/Chat/Body/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { MdComputer } from 'react-icons/md';
 
 import { useChatScreenState } from '../../../contexts/chat-screen';
 
@@ -10,13 +11,30 @@ const Body: React.FC = () => {
   return (
     <section className="body">
       {display && (
-        <>
+        <div className="body__chat">
           <header className="body__header">Body header</header>
           <section className="body__messages">Body messages</section>
           <div className="body__text-area">Body text area</div>
-        </>
+        </div>
       )}
-      <h1>Welcome</h1>
+      <div className="body__screen">
+        <img
+          src="assets/images/whatsapp-initial-connection.jpg"
+          alt="Whatsapp connection"
+          className="body__initial-img"
+        />
+        <h1 className="body__initial-title">Keep your phone connected</h1>
+        <p className="body__initial-text">
+          WhatsApp connects to your phone to sync message. To reduce data usage, connect your
+          phone to Wi-Fi.
+        </p>
+        <hr />
+        <MdComputer className="body__initial-icon" />
+        <span className="body__initial-span">
+          WhatsApp is available for Windows.
+          <a href="/" className="body__initial-windows-link">Get it here.</a>
+        </span>
+      </div>
     </section>
   );
 };
