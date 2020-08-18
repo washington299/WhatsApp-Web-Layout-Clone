6223b6e995fc4c466ad3f6d590cc9ba43980695d
Washington Campos <washingtoncampos9@gmail.com>
2020-08-17 22:33:06 -0300

    feat: create contacts context

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import { ChatScreenProvider } from './contexts/chat-screen';
+import { ContactsDataProvider } from './contexts/contacts';
 
 import Home from './pages/Home';
 import Chat from './pages/Chat';
@@ -11,12 +12,14 @@ import './App.scss';
 function App() {
   return (
     <ChatScreenProvider>
-      <Router>
-        <Switch>
-          <Route path="/chat" component={Chat} />
-          <Route exact path="/" component={Home} />
-        </Switch>
-      </Router>
+      <ContactsDataProvider>
+        <Router>
+          <Switch>
+            <Route path="/chat" component={Chat} />
+            <Route exact path="/" component={Home} />
+          </Switch>
+        </Router>
+      </ContactsDataProvider>
     </ChatScreenProvider>
   );
 }
---
+++ src/contexts/contacts.tsx
@@ -0,0 +1,59 @@
+import React, {
+  createContext, useReducer, useContext,
+} from 'react';
+
+import { IContacts } from '../interface';
+
+const initialData = {
+  name: '',
+  avatar: '',
+  lastMessage: '',
+  timeLastMessage: '',
+  lastSeen: '',
+  pinned: undefined,
+  mute: undefined,
+  unreadMessages: undefined,
+};
+
+const ContactsDataStateContext = createContext<IContacts>(initialData);
+const ContactsDataDispatchContext = createContext<React.Dispatch<Action>>(() => ({}));
+
+type Action =
+  | { type: 'GET_CONTACTS_DATA' }
+  | { type: 'SET_CONTACTS_DATA', payload: IContacts };
+
+function reducer(state: IContacts, action: Action) {
+  switch (action.type) {
+    case 'GET_CONTACTS_DATA':
+      return { ...state };
+    case 'SET_CONTACTS_DATA':
+      return {
+        ...state,
+        name: action.payload.name,
+        avatar: action.payload.avatar,
+        lastMessage: action.payload.lastMessage,
+        timeLastMessage: action.payload.timeLastMessage,
+        lastSeen: action.payload.lastSeen,
+        pinned: action.payload.pinned,
+        mute: action.payload.mute,
+        unreadMessages: action.payload.unreadMessages,
+      };
+    default:
+      return state;
+  }
+}
+
+export const ContactsDataProvider = ({ children }: any) => {
+  const [state, dispatch] = useReducer(reducer, initialData);
+
+  return (
+    <ContactsDataDispatchContext.Provider value={dispatch}>
+      <ContactsDataStateContext.Provider value={state}>
+        { children }
+      </ContactsDataStateContext.Provider>
+    </ContactsDataDispatchContext.Provider>
+  );
+};
+
+export const useContactsState = () => useContext(ContactsDataStateContext);
+export const useContactsDispatch = () => useContext(ContactsDataDispatchContext);
