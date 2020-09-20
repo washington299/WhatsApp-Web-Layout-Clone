e4282aa5ae20790421d443dc95c45e903dbd81d4
Washington <washingtoncampos9@gmail.com>
2020-09-20 03:46:46 -0300

    feat: create message box and make it responsive

---
+++ src/components/MessageBox/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './styles.scss';
+
+const MessageBox = () => (
+  <div className="message">
+    <div className="message__item">
+      <div className="message__text">In ad voluptate sit Lorem enim ullamco magna pariatur deserunt. Laborum adipisicing dolore eiusmod ad minim fugiat. Quis et ad qui ea minim esse eiusmod deserunt laborum nulla ex velit culpa deserunt. Non consectetur pariatur Lorem id cupidatat aute ex dolore proident elit.</div>
+      <div className="message__date">19:50</div>
+    </div>
+  </div>
+);
+
+export default MessageBox;
