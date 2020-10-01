60f0f8bf1ab4d4a6a119318a76fc5ffdcd42eb7a
Washington <washingtoncampos9@gmail.com>
2020-10-01 02:12:53 -0300

    refactor: display img shadow on mouse enter

--- src/components/SidebarModal/Profile/index.tsx
+++ src/components/SidebarModal/Profile/index.tsx
@@ -1,44 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Avatar } from '@material-ui/core';
 import { CameraAlt, Edit } from '@material-ui/icons';
 
 import './styles.scss';
 
-const Profile = () => (
-  <section className="profile">
-    <div className="profile__img-area">
-      <span className="profile__shadow">
-        <div className="profile__shadow-content">
-          <CameraAlt className="profile__icon--camera" />
-          <span className="profile__shadow-text">
-            Change profile
-            {' '}
-            <br />
-            {' '}
-            photo
-          </span>
+const Profile = () => {
+  const [displayShadow, setDisplayShadow] = useState(false);
+
+  return (
+    <section className="profile">
+      <div className="profile__img-area">
+        <span
+          className="profile__shadow"
+          style={displayShadow ? { display: 'flex' } : { display: 'none' }}
+          onMouseLeave={() => setDisplayShadow(false)}
+        >
+          <div className="profile__shadow-content">
+            <CameraAlt className="profile__icon--camera" />
+            <span className="profile__shadow-text">
+              Change profile
+              {' '}
+              <br />
+              {' '}
+              photo
+            </span>
+          </div>
+        </span>
+        <span onMouseEnter={() => setDisplayShadow(true)}>
+          <Avatar className="profile__img" />
+        </span>
+      </div>
+      <div className="profile__box">
+        <span className="profile__title">Your name</span>
+        <div className="profile__info">
+          <input type="text" defaultValue="Washington Campos" className="profile__input" />
+          <Edit className="profile__icon--edit" />
         </div>
-      </span>
-      <Avatar className="profile__img" />
-    </div>
-    <div className="profile__box">
-      <span className="profile__title">Your name</span>
-      <div className="profile__info">
-        <input type="text" defaultValue="Washington Campos" className="profile__input" />
-        <Edit className="profile__icon--edit" />
       </div>
-    </div>
-    <div className="profile__text">
-      This is not your username or pin. This name will be visible to your WhatsApp contacts
-    </div>
-    <div className="profile__box">
-      <span className="profile__title">About</span>
-      <div className="profile__info">
-        <input type="text" defaultValue="In the end it doesn't even matter" className="profile__input" />
-        <Edit className="profile__icon--edit" />
+      <div className="profile__text">
+        This is not your username or pin. This name will be visible to your WhatsApp contacts
       </div>
-    </div>
-  </section>
-);
+      <div className="profile__box">
+        <span className="profile__title">About</span>
+        <div className="profile__info">
+          <input type="text" defaultValue="In the end it doesn't even matter" className="profile__input" />
+          <Edit className="profile__icon--edit" />
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default Profile;
