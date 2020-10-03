e26c4fdf597ac2728f98fe0251c0a801190dd839
Washington <washingtoncampos9@gmail.com>
2020-10-03 19:46:18 -0300

    feat: create new chat search area

--- src/components/SidebarModal/NewChat/index.tsx
+++ src/components/SidebarModal/NewChat/index.tsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import { Search, GroupAdd } from '@material-ui/icons';
+
+import FindContacts from '../FindContacts';
+
+import './styles.scss';
 
 const NewChat = () => (
-  <div>New Chat</div>
+  <section className="newChat">
+    <div className="newChat__search">
+      <div className="newChat__search-area">
+        <Search className="newChat__icon--search" />
+        <input type="text" placeholder="Search contacts" className="newChat__input" />
+      </div>
+    </div>
+    <div className="newChat__newGroup-add">
+      <GroupAdd className="newChat__icon--group-add" />
+      <div className="newChat__text">New Group</div>
+    </div>
+    <FindContacts />
+  </section>
 );
 
 export default NewChat;
